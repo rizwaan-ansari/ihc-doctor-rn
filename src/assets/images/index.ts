038799211075d5351db8f1e502048a1fe4d68fac
rizwaan-ansari <rizwaan@oromedialab.com>
2024-09-12 13:11:15 +0530

    Added Assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,132 @@
+export const IMG_DIR = '/images';
+
+export const IMG_LANGUAGE_ICON = './translate-icon.png';
+export const IMG_LANGUAGE_ICON_2 = './translate-icon-2.png';
+export const IMG_LANGUAGE_FLAG_1 = require('./img-flag-1.png');
+export const IMG_LANGUAGE_FLAG_2 = require('./img-flag-2.png');
+
+export const ICON_HOME = require('./home.png');
+export const ICON_CALENDAR = require('./calendar.png');
+export const ICON_SEARCH = require('./search.png');
+export const ICON_WALLET = require('./wallet.png');
+export const ICON_HORIZONTAL_DOTS = require('./horizontal-dots.png');
+
+
+export const IMG_ADD_BANNER_1 = require('./add-banner-1.png');
+export const IMG_ADD_BANNER_2 = require('./add-banner-2.png');
+export const IMG_ADD_BANNER_3 = require('./add-banner-3.png');
+export const IMG_ADD_BANNER_4 = require('./add-banner-4.png');
+export const IMG_ADD_BANNER_5 = require('./add-banner-5.png');
+
+export const IMG_CARDIO_ICON = require('./cardio-icon.png');
+export const IMG_DENTIST_ICON = require('./dentist-icon.png');
+export const IMG_DERMATOLOGY_ICON = require('./dermatology-icon.png');
+export const IMG_UROLOGY_ICON = require('./urology-icon.png');
+export const IMG_ENT_ICON = require('./ent-icon.png');
+
+
+export const IMG_CARDIOLOGY_THEME_ICON = require('./cardiology-theme-icon.png');
+export const IMG_DENTISTRY_THEME_ICON = require('./dentistry-theme-icon.png');
+export const IMG_DERMATOLOGY_THEME_ICON = require('./dermatology-theme-icon.png');
+export const IMG_ENT_THEME_ICON = require('./ent-theme-icon.png');
+export const IMG_UROLOGY_THEME_ICON = require('./urology-theme-icon.png');
+
+export const IMG_USER_PROFILE_1 = require('./user-profile-icon-1.png');
+export const IMG_USER_PROFILE_2 = require('./user-profile-icon-2.png');
+export const IMG_USER_PROFILE_3 = require('./user-profile-icon-3.png');
+export const IMG_USER_PROFILE_4 = require('./user-profile-icon-4.png');
+
+export const IMG_LOGIN_ICON = require('./login-icon.png');
+
+export const IMG_WALKIN_ICON = require('./walkin-icon.png');
+export const IMG_VIDEO_ICON = require('./video-icon.png');
+export const IMG_CALENDAR_ICON = require('./calendar-icon.png');
+export const IMG_VIDEO_ICON_CIRCLE = require('./video-icon-circle.png');
+export const IMG_TIMESLOT_ICON_CIRCLE = require('./timeslot-icon-circle.png');
+export const IMG_CLOCK_ICON_CIRCLE = require('./clock-icon-circle.png');
+
+export const IMG_MORNING_ICON = require('./morning.png');
+export const IMG_AFTERNOON_ICON = require('./afternoon.png');
+export const IMG_EVENING_ICON = require('./evening.png');
+
+export const IMG_PATIENT_PFP_1 = require('./inaya-patient-pfp.png');
+export const IMG_PAYMENT_STATUS_SUCCESS = require('./payment-success.png');
+
+export const IMG_LOGO = require('./logo.png');
+export const IMG_IPHONE_ICON = require('./iphone-icon.png');
+export const IMG_GOOGLE_ICON = require('./google-icon.png');
+export const IMG_FACEBOOK_ICON = require('./facebook-icon.png');
+
+export const IMG_CONFIRM_PASSWORD_BANNER = require('./confirm-password-banner.png');
+export const IMG_PATIENT_PROFILE_ICON = require('./patient-profile.png');
+
+export const IMG_VIDEO_CALL_ICON = require('./video-call-icon.png');
+export const IMG_WALK_IN_CALL_ICON = require('./walk-in-icon.png');
+
+export const IMG_APPOINTMENT_MENU_ICON = require('./appointments-icon.png');
+export const IMG_PATIENTS_MENU_ICON = require('./patients-icon.png');
+export const IMG_NOTIFICATIONS_MENU_ICON = require('./notifications-icon.png');
+export const IMG_AVAILABILITY_MENU_ICON = require('./availability-icon.png');
+export const IMG_TIME_BLOCK_MENU_ICON = require('./time-block-icon.png');
+export const IMG_WALLET_MENU_ICON = require('./wallet-icon.png');
+export const IMG_REVIEWS_MENU_ICON = require('./reviews-icon.png');
+export const IMG_LANGUAGE_MENU_ICON = require('./language-icon.png');
+export const IMG_MY_PROFILE_MENU_ICON = require('./my-profile-icon.png');
+
+export const IMG_ABOUT_ICON_MORE = require('./about-icon-(more).png');
+export const IMG_PRIVACY_POLICY_ICON_MORE = require('./privacy-policy-icon-(more).png');
+export const IMG_TERMS_CONDITIONS_ICON_MORE = require('./terms-condition-icon-(more).png');
+export const IMG_SECURITY_ICON_MORE = require('./security-icon-(more).png');
+export const IMG_CONTACT_US_ICON_MORE = require('./contact-us-icon-(more).png');
+export const IMG_FAQ_ICON_MORE = require('./faq-icon-(more).png');
+export const IMG_LOGIN_ICON_MORE = require('./login-icon-(more).png');
+export const IMG_REGISTER_ICON_MORE = require('./register-icon-(more).png');
+
+export const IMG_PATIENT_2_PROFILE_ICON = require('./patient-profile-2.png');
+export const IMG_PATIENT_3_PROFILE_ICON = require('./patient-profile-3.png');
+export const IMG_PATIENT_4_PROFILE_ICON = require('./patient-profile-4.png');
+
+export const IMG_CELL_PHONE_ICON = require('./phone-icon.png');
+export const IMG_PASSWORD_LOCK_ICON = require('./password-icon.png');
+
+export const IMG_FILTER_LIST_ICON = require('./filter-list-icon.png');
+export const IMG_FILTER_MENU_ICON = require('./filter-menu-icon.png');
+export const IMG_FILTER_VIDEO_CALL_ICON = require('./filter-video-call-icon.png');
+export const IMG_FILTER_ICON = require('./filter-icon.png');
+export const IMG_FILTER_ICON_2 = require('./filter-icon-2.png');
+
+export const IMG_EMPTY_ICON = require('./empty-icon.png');
+export const IMG_KD_WATERMARK = require('./payment-watermark.png');
+
+export const IMG_PRESCRIPTION_ICON = require('./prescription-icon.png');
+export const IMG_REPORT_ICON = require('./report-icon.png');
+export const IMG_NOTES_ICON = require('./notes-icon.png');
+
+export const IMG_DR_CHRISTIAN_HICKMAN = require('./dr-christian-hickman.png');
+export const IMG_DR_AUSTIN_LANE = require('./dr-austin-lane.png');
+export const IMG_DR_HAMID_FAREED_AHMED = require('./dr-hamid-fareed-ahmed.png');
+
+export const IMG_SEARCH_ICON = require('./search-icon.png');
+export const IMG_TABLET_ICON = require('./tablet-icon.png');
+
+export const IMG_PRESCRIPTION_UPLOAD = require('./prescription-upload.png');
+
+export const IMG_REPORT_1 = require('./report-1.png');
+export const IMG_REPORT_2 = require('./report-2.png');
+export const IMG_REPORT_3 = require('./report-3.png');
+export const IMG_REPORT_4 = require('./report-4.png');
+export const IMG_REPORT_5 = require('./report-5.png');
+export const IMG_REPORT_6 = require('./report-6.png');
+export const IMG_REPORT_7 = require('./report-7.png');
+export const IMG_REPORT_8 = require('./report-8.png');
+
+export const IMG_VIDEOCALL_LADY = require('./video-call-lady.png');
+export const IMG_VIDEOCALL_LADY_2 = require('./video-call-lady-2.png');
+export const IMG_MESSAGE_ICON = require('./message-icon.png');
+export const IMG_SUITCASE_ICON = require('./suitcase-icon.png');
+
+export const IMG_MESSAGE_AVATAR = require('./message-avatar.png');
+
+export const IMG_REQUEST_SUCCESSFUL = require('./request-successful-icon.png');
+export const IMG_WITHDRAWAL_SUCCESSFUL = require('./withdrawal-successful-icon.png');
+export const IMG_TRANSALTE_ICON = require('./translate-icon.png');
